f29e7e3d4c77e6a591eb38333b4203627dd9bcd0
Niallghus <74742450+BenjaminStimec@users.noreply.github.com>
2023-04-05 01:46:23 +0200

    init

---
+++ src/main/java/com/example/dragonhackspringbackend/DragonHackSpringBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.dragonhackspringbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DragonHackSpringBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DragonHackSpringBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dragonhackspringbackend/DragonHackSpringBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dragonhackspringbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DragonHackSpringBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
